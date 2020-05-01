c9c141fcd3d01df150f09f47e7c29dd4dcd7400d
Higor Oliveira <higorpiresoliveira@gmail.com>
2020-04-30 22:57:58 -0300

    feat: Home Screen, add clock

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Image, View } from 'react-native';
 import AnalogClock from 'react-native-clock-analog';
-import { Container, Header } from './styles';
+import { Container, Header, ClockerContainer, HourLabel, DateLabel } from './styles';
+import { format, compareAsc } from 'date-fns'
+import brLocale from 'date-fns/locale/pt-BR';
 
 const logo = require("../../../assets/logo.png");
 
+
 export default function Home() {
+    /**
+     * State
+     */
+    const [hour, setHour] = useState<string>("00:00");
+    const [date, setDate] = useState<string>("Quarta-feira, 26/03");
+
+    useEffect(() => {
+        setHour(format(new Date(), "HH:mm", { locale: brLocale }));
+        setDate(format(new Date(), "EEEE, dd", { locale: brLocale }));
+    }, [])
+
     return (
         <Container>
             <Header>
@@ -13,15 +27,22 @@ export default function Home() {
                     source={logo}
                 />
             </Header>
-            <AnalogClock
-                colorClock="#6D216A"
-                colorNumber="white"
-                colorCenter="white"
-                colorHour="#ccc"
-                colorMinutes="#eee"
-                hour="2"
-                minutes={55}
-            />
+            <ClockerContainer>
+                <AnalogClock
+                    key="1"
+                    colorClock="#6D216A"
+                    colorNumber="white"
+                    colorCenter="white"
+                    colorHour="#ccc"
+                    colorMinutes="#eee"
+                    hour={new Date().getHours()}
+                    minutes={new Date().getMinutes()}
+                />
+                <View style={{ marginLeft: 20 }}>
+                    <HourLabel>{hour}</HourLabel>
+                    <DateLabel>{date}</DateLabel>
+                </View>
+            </ClockerContainer>
         </Container>
     );
 }
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -11,3 +11,20 @@ export const Header = styled.View`
     justify-content: center;
     margin-top: 60px;
 `;
+
+export const ClockerContainer = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-top: 30px;
+`
+
+export const HourLabel = styled.Text`
+    color: white;
+    font-size: 40px;
+    font-weight: bold;
+`;
+export const DateLabel = styled.Text`
+    color: white;
+    margin-top: -5px;
+`
