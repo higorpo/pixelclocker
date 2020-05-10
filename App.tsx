27d368a1d1808f1fefeb110c77e31c4932354845
Higor Oliveira <higorpiresoliveira@gmail.com>
2020-05-10 19:55:48 -0300

    feat: themeing

--- App.tsx
+++ App.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { Theme } from '@react-navigation/native/lib/typescript/src/types';
 
 import { store, persistor } from './src/store';
 
 import Routes from './src/routes';
 
+const CustomTheme: Theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#82327E'
+  },
+};
+
 export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <NavigationContainer>
+        <NavigationContainer theme={CustomTheme}>
           <Routes />
         </NavigationContainer>
       </PersistGate>
